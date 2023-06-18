25f9d228f3c704877fc750c2376ff959aec83259
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-06-18 04:57:14 -0300

    Added the cronn core to call the project API

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from Api.CheckDateGet import GetDate
+from Api.GetIpca import getIpca
 from Json.JsonHandler import json_load
 from Helpers.StringHelper import check_string_in_list
 import time
@@ -20,4 +21,11 @@ while True:
     isCopomDate = check_string_in_list(todayDateString,dateList)
     isIpcaDate = check_string_in_list(todayDateString,ipcaDates)
     time.sleep(3)
-    #if isIpcaDate == True:
+    
+    if isIpcaDate == True:
+        try:
+            response, statusCode = getIpca()
+        except Exception as error:
+            print("Sorry we couldn't get the IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
+            
+        
