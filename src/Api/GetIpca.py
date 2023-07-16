055189052b06ab1f25ed9725855370d938906e1e
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-07-15 23:15:47 -0300

    Separeted try/catch for each endpoint check

--- src/Api/GetIpca.py
+++ src/Api/GetIpca.py
@@ -1,8 +1,22 @@
 import requests
 from Json.JsonHandler import json_load
-from datetime import datetime
 
-def getIpca():
-    url = json_load("Settings\settings.json")['url'][0]['Bc.Watcher.Api'] 
+def monitoredPrices():
+    url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/PrecosMonitoradosTotal'] 
+    responseRaw = requests.request("GET", url, headers={}, data={})
+    return responseRaw.json(), responseRaw.status_code
+
+def durableGoods():
+    url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/DurableGoods'] 
+    responseRaw = requests.request("GET", url, headers={}, data={})
+    return responseRaw.json(), responseRaw.status_code
+
+def nonDurableGoods():
+    url = json_load("Settings\settings.json")['endpoints'][0]['c.Watcher.Api/NonDurableGoods'] 
+    responseRaw = requests.request("GET", url, headers={}, data={})
+    return responseRaw.json(), responseRaw.status_code
+
+def services():
+    url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/Services'] 
     responseRaw = requests.request("GET", url, headers={}, data={})
     return responseRaw.json(), responseRaw.status_code
--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from Api.CheckDateGet import GetDate
-from Api.GetIpca import getIpca
+from Api.GetIpca import monitoredPrices, durableGoods, nonDurableGoods, services
 from Json.JsonHandler import json_load
 from Helpers.StringHelper import check_string_in_list
 import time
+import polars as pl
+
+
 copomDates = json_load("Settings\Copom_dates.json")
 ipcaDates = json_load("Settings\Ipca_dates.json")
 ipcaDates = ipcaDates['dates']
@@ -17,15 +20,31 @@ for itens in copomDates['dates']:
 
 while True:
     todayDateObject, todayDateString, status  = GetDate()
-    print(status)
     isCopomDate = check_string_in_list(todayDateString,dateList)
     isIpcaDate = check_string_in_list(todayDateString,ipcaDates)
+    print(isCopomDate, isIpcaDate)
+    print(todayDateString)
     time.sleep(3)
     
     if isIpcaDate == True:
         try:
-            response, statusCode = getIpca()
+            response_monitored_prices, statusCode = monitoredPrices()
         except Exception as error:
-            print("Sorry we couldn't get the IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
-            
+            print("Sorry we couldn't get the monitored IPCA Prices, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
         
+        try:
+            durableGoodsResponse, durableGoodsResponseStatusCode = durableGoods()
+        except Exception as error:
+            print("Sorry we couldn't get the monitored durable goods IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
+        
+        # try:
+        #     nonDurableGoodsResponse, nonDurableGoodsStatusCode = nonDurableGoods()
+        # except Exception as error:
+        #     print("Sorry we couldn't get the monitored non durable goods IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
+        
+        try:
+            servicesResponse, servicesStatusCode = services()
+        except Exception as error:
+            print("Sorry we couldn't get the monitored durable service IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
+        
+        print(response_monitored_prices, durableGoodsResponse, servicesResponse)
