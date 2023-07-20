4401bf1d7358ec3f68a98c90be4cb1daf11012d6
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-07-19 23:38:17 -0300

    Implemented the durablegoods endpoint call

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ while True:
             if (todayDateObject==lastValues.iloc[0,1]) == False:
                 try:
                     sendMessage("Olá parece que identificamos uma novo valor de: IPCA monitorados")
-                    responseMonitoredPrices, statusCode = monitoredPrices(13)
+                    responseMonitoredPrices, monitoredPricesStatusCode = monitoredPrices(13)
                     sendMessage(str(responseMonitoredPrices[12]))
                     lastValues.iloc[0,1] = todayDateObject
                     lastValues.to_excel("last_values.xlsx", index=False)
@@ -47,12 +47,19 @@ while True:
         except Exception as error:
             print("Sorry we couldn't get the monitored IPCA Prices, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
         
-        #The above logic works now is time to function the hell of it and apply to the order endpoints
         
-        # try:
-        #     durableGoodsResponse, durableGoodsResponseStatusCode = durableGoods()
-        # except Exception as error:
-        #     print("Sorry we couldn't get the monitored durable goods IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
+        try:
+            responseDurableGoods, durableGoodsResponseStatusCode = durableGoods(1)
+            responseDurableGoods = responseDurableGoods[0]
+            if(todayDateObject == lastValues.iloc[1,1]) == False:
+                sendMessage("Olá parece que identificamos uma novo valor de: IPCA para bens duárveis")
+                responseDurableGoods, durableGoodsStatusCode = durableGoods(13)
+                sendMessage(str(responseDurableGoods[12]))
+                lastValues.iloc[1,1] = todayDateObject
+                lastValues.to_excel("last_values.xlsx", index=False)
+            
+        except Exception as error:
+            print("Sorry we couldn't get the monitored durable goods IPCA, we meet the following problems: " + str(error) + "API status code were: "+ str(statusCode))
         
         # try:
         #     nonDurableGoodsResponse, nonDurableGoodsStatusCode = nonDurableGoods()
