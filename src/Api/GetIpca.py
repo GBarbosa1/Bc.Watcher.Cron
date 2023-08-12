86b341c7d2f86b055f219006fc42b3d8cfe2d7ef
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-08-12 12:51:05 -0300

    Implemented the get record link

--- src/Api/GetIpca.py
+++ src/Api/GetIpca.py
@@ -8,15 +8,15 @@ def monitoredPrices(last):
 
 def durableGoods(last):
     url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/DurableGoods'] + str(last)
-    responseRaw = requests.request("GET", url, headers={}, data={})
+    responseRaw = requests.request("GET", url, headers={}, data={}).json()
     return responseRaw
 
-# def nonDurableGoods(last):
-#     url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/NonDurableGoods'] + str(last)
-#     responseRaw = requests.request("GET", url, headers={}, data={})
-#     return responseRaw.json(), responseRaw.status_code
+def nonDurableGoods(last):
+    url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/NonDurableGoods'] + str(last)
+    responseRaw = requests.request("GET", url, headers={}, data={}).json()
+    return responseRaw
 
-# def services(last):
-#     url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/Services'] + str(last)
-#     responseRaw = requests.request("GET", url, headers={}, data={})
-#     return responseRaw.json(), responseRaw.status_code
+def services(last):
+    url = json_load("Settings\settings.json")['endpoints'][0]['Bc.Watcher.Api/Services'] + str(last)
+    responseRaw = requests.request("GET", url, headers={}, data={}).json()
+    return responseRaw
